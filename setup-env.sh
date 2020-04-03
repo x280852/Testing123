0751be6c451ffb345c55a3719427346efe41ea73
x280852 <60961374+x280852@users.noreply.github.com>
2020-04-03 09:06:27 +0200

    Update setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -2,3 +2,5 @@ echo "Setting environment variables for Terraform"
 export ARM_CLIENT_ID=$servicePrincipalId
 export ARM_CLIENT_SECRET=$servicePrincipalKey
 export ARM_TENANT_ID="35595a02-4d6d-44ac-99e1-f9ab4cd872db"
+export ARM_SUBSCRIPTION_ID=$SUBSCRIPTION_ID
+
