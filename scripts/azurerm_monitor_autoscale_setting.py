b44e2d68c1459664351485eaf6ae712eac07dafc
x280852 <60961374+x280852@users.noreply.github.com>
2020-05-19 09:03:43 +0200

    Update azurerm_monitor_autoscale_setting.py

--- scripts/azurerm_monitor_autoscale_setting.py
+++ scripts/azurerm_monitor_autoscale_setting.py
@@ -73,7 +73,7 @@ def azurerm_monitor_autoscale_setting(crf,cde,crg,headers,requests,sub,json,az2t
                 # case sensitite so use actual ID
                 #fr.write('target_resource_id = "' + triid + '"\n')
                 #fr.write('target_resource_id = "${'+ tftyp + '.' + trrg + '__' + trid+'.id}"\n')
-                fr.write('target_resource_id = "${'+ tftyp + '.' + trrg + '__' + trid+'}"\n')
+                fr.write('target_resource_id = "01234567890"\n')
             except KeyError:
                 pass
 
@@ -177,7 +177,7 @@ def azurerm_monitor_autoscale_setting(crf,cde,crg,headers,requests,sub,json,az2t
                                 fr.write('\t\t\tmetric_name = "' + mtn + '"\n')
                                 fr.write(
                                     #'\t\t\tmetric_resource_id = '+tftyp + '.' + mtrrg + '__' + mtrid + '.id\n')
-                                    '\t\t\tmetric_resource_id = '+tftyp + '.' + mtrrg + '__' + mtrid + '\n')
+                                    '\t\t\tmetric_resource_id = "01234567890" + '\n')
                                 fr.write('\t\t\toperator = "' + mtop + '"\n')
                                 fr.write('\t\t\tstatistic= "' + mtstat + '"\n')
                                 fr.write('\t\t\tthreshold = "' +
